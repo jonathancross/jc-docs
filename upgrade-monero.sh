0768a19f83fae033a666b3f3bbc5b84fb4e686bb
Jonathan Cross <jcross@gmail.com>
2019-11-23 16:33:16 +0100

    upgrade-monero.sh - tested with v0.15.0.1

--- upgrade-monero.sh
+++ upgrade-monero.sh
@@ -2,7 +2,7 @@
 #
 # Bash script used to install the newest version of the Monero cli on Linux.
 # Script will properly validate downloads, verify gpg signatures and checksums.
-# Tested with v0.11 - v0.15.0.0
+# Tested with v0.11 - v0.15.0.1
 #
 # REQUIREMENTS:
 #
